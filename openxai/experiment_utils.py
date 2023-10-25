b2a6b398836e2198063fd73c1e7e4317ce75ac99
danwley <d.w.ley@hotmail.com>
2023-10-25 12:12:29 -0400

    fix generate_mask function to a) set topk features to False and b) consider topk based on absolute value

--- openxai/experiment_utils.py
+++ openxai/experiment_utils.py
@@ -29,10 +29,12 @@ tqdm.__init__ = partialmethod(tqdm.__init__, disable=True)
 
 
 def generate_mask(explanation, top_k):
-    mask_indices = torch.topk(explanation, top_k).indices
-    mask = torch.zeros(explanation.shape) > 10
+    if not isinstance(explanation, torch.Tensor):
+        explanation = torch.Tensor(explanation)
+    mask_indices = torch.topk(explanation.abs(), top_k).indices
+    mask = torch.ones(explanation.shape, dtype=bool)
     for i in mask_indices:
-        mask[i] = True
+        mask[i] = False
     return mask
 
 
