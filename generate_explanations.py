e5d236524741513331129e0c4bdd176a98860575
danwley <d.w.ley@hotmail.com>
2024-02-19 00:44:20 -0500

    script to generate and save post hoc explanations

---
+++ generate_explanations.py
@@ -0,0 +1,42 @@
+# Utils
+import os
+import torch
+import numpy as np
+
+# ML models
+from openxai.model import LoadModel
+
+# Data loaders
+from openxai.dataloader import return_loaders
+
+# Explanation models
+from openxai.explainer import Explainer
+
+def GenerateExplanations(methods, data_name, model_name, n_test_samples=100):
+    # Get data and model
+    loader_train, loader_test = return_loaders(data_name=data_name, download=False)
+    inputs = torch.FloatTensor(loader_test.dataset.data[:n_test_samples])
+    labels = torch.LongTensor(loader_test.dataset.targets[:n_test_samples])
+    dataset_tensor = torch.FloatTensor(loader_train.dataset.data)
+    model = LoadModel(data_name, model_name, pretrained=True)
+    
+    # Compute explanations for each method
+    explanations = {method: None for method in methods}
+    for method in methods:
+        explainer = Explainer(method, model, dataset_tensor, param_dict=None)  # use default hyperparameters in openxai.explainer.py
+        explanations[method] = explainer.get_explanation(inputs, labels)
+    return explanations
+
+if __name__ == '__main__':
+    if not os.path.exists('explanations'):
+        os.makedirs('explanations')
+    methods = ['control', 'grad', 'ig', 'itg', 'sg', 'shap', 'lime']
+    data_names = ['adult', 'compas', 'gaussian', 'german', 'gmsc', 'heart', 'heloc', 'pima']
+    model_names = ['lr', 'ann']
+    n_test_samples = 500
+    for data_name in data_names:
+        for model_name in model_names:
+            explanations = GenerateExplanations(methods, data_name, model_name, n_test_samples)
+            for method, explanation in explanations.items():
+                filename = f'explanations/{data_name}_{model_name}_{method}_{n_test_samples}.npy'
+                np.save(filename.format(filename), explanation.detach().numpy())
