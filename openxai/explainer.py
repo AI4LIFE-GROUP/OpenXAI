849a898800efc8858884133f7a83e31583ca8211
danwley <d.w.ley@hotmail.com>
2024-02-18 21:14:56 -0500

    refactored explainer.py

--- openxai/explainer.py
+++ openxai/explainer.py
@@ -11,78 +11,87 @@ from openxai.explainers import LIME
 from openxai.explainers import SHAPExplainerC
 from openxai.explainers import RandomBaseline
 
+explainers_dict = {
+    'grad': Gradient,
+    'sg': SmoothGrad,
+    'itg': InputTimesGradient,
+    'ig': IntegratedGradients,
+    'shap': SHAPExplainerC,
+    'lime': LIME,
+    'control': RandomBaseline
+}
+
+default_param_dicts = {
+    'grad': {
+        'absolute_value': True
+    },
+    'sg': {
+        'num_samples': 100,
+        'standard_deviation': 0.005
+    },
+    'itg': {},
+    'ig': {
+        'method': 'gausslegendre',
+        'multiply_by_inputs': False
+    },
+    'shap': {
+        'model_impl': 'torch',
+        'n_samples': 500
+    },
+    'lime': {
+        'kernel_width': 0.75,
+        'std': float(np.sqrt(0.03)),
+        'mode': 'tabular',
+        'sample_around_instance': True,
+        'n_samples': 1000,
+        'discretize_continuous': False
+    },
+    'control': {}
+}
+
+def fill_param_dict(method: str, param_dict: dict, dataset_tensor: torch.tensor):
+    """
+    Fills in the missing parameters for the given method with the default parameters
+    :param method: str, name of the method
+    :param param_dict: dict, parameter dictionary
+    :return: dict, filled parameter dictionary
+    """
+    # None case
+    param_dict = {} if param_dict is None else param_dict
+    
+    # Parameters requiring variables from IG and LIME 
+    if (method == 'ig') and ('baseline' not in param_dict):
+        param_dict['baseline'] = torch.mean(dataset_tensor, dim=0).reshape(1, -1).float()
+    elif (method == 'lime') and ('dataset_tensor' not in param_dict):
+        param_dict['data'] = dataset_tensor
+
+    # Fill in missing parameters
+    default_param_dict = default_param_dicts[method]
+    for key in default_param_dict.keys():
+        if key not in param_dict:
+            param_dict[key] = default_param_dict[key]
+    return param_dict
 
 def Explainer(method: str,
               model,
               dataset_tensor: torch.tensor,
-              param_dict_grad=None,
-              param_dict_sg=None,
-              param_dict_ig=None,
-              param_dict_lime=None,
-              param_dict_shap=None):
-    
-    if method == 'grad':
-        if param_dict_grad is None:
-            param_dict_grad = dict()
-            param_dict_grad['absolute_value'] = True
-        explainer = Gradient(model,
-                             absolute_value=param_dict_grad['absolute_value'])
-    
-    elif method == 'sg':
-        if param_dict_sg is None:
-            param_dict_sg = dict()
-            param_dict_sg['n_samples'] = 100
-            param_dict_sg['standard_deviation'] = 0.005
-        explainer = SmoothGrad(model,
-                               num_samples=param_dict_sg['n_samples'],
-                               standard_deviation=param_dict_sg['standard_deviation'])
-    
-    elif method == 'itg':
-        explainer = InputTimesGradient(model)
-    
-    elif method == 'ig':
-        if param_dict_ig is None:
-            param_dict_ig = dict()
-            param_dict_ig['method'] = 'gausslegendre'
-            param_dict_ig['multiply_by_inputs'] = False
-            param_dict_ig['baseline'] = torch.mean(dataset_tensor, dim=0).reshape(1, -1).float()
-        explainer = IntegratedGradients(model,
-                                        method=param_dict_ig['method'],
-                                        multiply_by_inputs=param_dict_ig['multiply_by_inputs'],
-                                        baseline=param_dict_ig['baseline'])
-    
-    elif method == 'shap':
-        if param_dict_shap is None:
-            param_dict_shap = dict()
-            param_dict_shap['subset_size'] = 500
-        explainer = SHAPExplainerC(model,
-                                   model_impl='torch',
-                                   n_samples=param_dict_shap['subset_size'])
-
-    elif method == 'lime':
-        if param_dict_lime is None:
-            param_dict_lime = dict()
-            param_dict_lime['dataset_tensor'] = dataset_tensor
-            param_dict_lime['kernel_width'] = 0.75
-            param_dict_lime['std'] = float(np.sqrt(0.05))
-            param_dict_lime['mode'] = 'tabular'
-            param_dict_lime['sample_around_instance'] = True
-            param_dict_lime['n_samples'] = 1000
-            param_dict_lime['discretize_continuous'] = False
-
-        explainer = LIME(model.predict,
-                         param_dict_lime['dataset_tensor'],
-                         std=param_dict_lime['std'],
-                         mode=param_dict_lime['mode'],
-                         sample_around_instance=param_dict_lime['sample_around_instance'],
-                         kernel_width=param_dict_lime['kernel_width'],
-                         n_samples=param_dict_lime['n_samples'],
-                         discretize_continuous=param_dict_lime['discretize_continuous'])
-
-    elif method == 'control':
-        explainer = RandomBaseline(model)
-    
-    else:
+              param_dict=None):
+    """
+    Returns an explainer object for the given method
+    :param method: str, name of the method
+    :param model: PyTorch model or function
+    :param dataset_tensor: torch.tensor, dataset tensor
+    :param param_dict: dict, parameter dictionary
+    :return: explainer object
+    """
+    # Verify method
+    if method not in explainers_dict.keys():
         raise NotImplementedError("This method has not been implemented, yet.")
-    
+
+    # Configure parameters
+    model = model.predict if method == 'lime' else model
+    param_dict = fill_param_dict(method, param_dict, dataset_tensor)
+
+    # Return explainer
+    explainer = explainers_dict[method](model, **param_dict)
     return explainer
