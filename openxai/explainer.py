1bce9a5e6695ec92757c221a50ff6e02c6ad5328
danwley <d.w.ley@hotmail.com>
2024-02-28 14:45:06 -0500

    remove typing, refactor imports and param dicts

--- openxai/explainer.py
+++ openxai/explainer.py
@@ -3,53 +3,25 @@ import torch
 import numpy as np
 
 # Explanation Models
-from openxai.explainers import Gradient
-from openxai.explainers import IntegratedGradients
-from openxai.explainers import InputTimesGradient
-from openxai.explainers import SmoothGrad
-from openxai.explainers import LIME
-from openxai.explainers import SHAPExplainerC
-from openxai.explainers import RandomBaseline
+from openxai.explainers import Gradient, IntegratedGradients,\
+    InputTimesGradient, SmoothGrad, LIME, SHAPExplainerC, RandomBaseline
 
 explainers_dict = {
-    'grad': Gradient,
-    'sg': SmoothGrad,
-    'itg': InputTimesGradient,
-    'ig': IntegratedGradients,
-    'shap': SHAPExplainerC,
-    'lime': LIME,
-    'control': RandomBaseline
+    'grad': Gradient, 'sg': SmoothGrad, 'itg': InputTimesGradient, 'ig': IntegratedGradients,
+    'shap': SHAPExplainerC, 'lime': LIME, 'control': RandomBaseline
 }
 
 default_param_dicts = {
-    'grad': {
-        'absolute_value': True
-    },
-    'sg': {
-        'n_samples': 100,
-        'standard_deviation': 0.005
-    },
-    'itg': {},
-    'ig': {
-        'method': 'gausslegendre',
-        'multiply_by_inputs': False
-    },
-    'shap': {
-        'model_impl': 'torch',
-        'n_samples': 500
-    },
-    'lime': {
-        'kernel_width': 0.75,
-        'std': float(np.sqrt(0.03)),
-        'mode': 'tabular',
-        'sample_around_instance': True,
-        'n_samples': 1000,
-        'discretize_continuous': False
-    },
-    'control': {}
+    'grad': {'absolute_value': True},
+    'sg': {'n_samples': 100, 'standard_deviation': 0.005},
+    'ig': {'method': 'gausslegendre', 'multiply_by_inputs': False},
+    'shap': {'model_impl': 'torch', 'n_samples': 500},
+    'lime': {'kernel_width': 0.75, 'std': float(np.sqrt(0.03)), 'mode': 'tabular',
+             'sample_around_instance': True, 'n_samples': 20000, 'discretize_continuous': False},
+    'itg': {}, 'control': {}
 }
 
-def fill_param_dict(method: str, param_dict: dict, dataset_tensor: torch.tensor):
+def fill_param_dict(method, param_dict, dataset_tensor):
     """
     Fills in the missing parameters for the given method with the default parameters
     :param method: str, name of the method
@@ -72,10 +44,7 @@ def fill_param_dict(method: str, param_dict: dict, dataset_tensor: torch.tensor)
             param_dict[key] = default_param_dict[key]
     return param_dict
 
-def Explainer(method: str,
-              model,
-              dataset_tensor: torch.tensor,
-              param_dict=None):
+def Explainer(method, model, dataset_tensor, param_dict=None):
     """
     Returns an explainer object for the given method
     :param method: str, name of the method
