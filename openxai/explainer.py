155be77fecedce88897b7726c28b7cdb1c42ab64
danwley <d.w.ley@hotmail.com>
2024-03-01 11:45:33 -0500

    move default parameters for explainers outside openxai library

--- openxai/explainer.py
+++ openxai/explainer.py
@@ -1,8 +1,4 @@
-# Utils
-import torch
-import numpy as np
-
-# Explanation Models
+# Explainers
 from openxai.explainers import Gradient, IntegratedGradients,\
     InputTimesGradient, SmoothGrad, LIME, SHAPExplainerC, RandomBaseline
 
@@ -16,77 +12,16 @@ explainers_dict = {
     'control': RandomBaseline
 }
 
-default_param_dicts = {
-    'grad': {
-        'absolute_value': False
-    },
-    'sg': {
-        'n_samples': 500,
-        'standard_deviation': 0.1,
-        'seed': 0
-    },
-    'ig': {
-        'method': 'gausslegendre', 
-        'multiply_by_inputs': False
-    },
-    'shap': {
-        'n_samples': 500,
-        'model_impl': 'torch',
-        'seed': 0,
-    },
-    'lime': {
-        'n_samples': 1000,
-        'kernel_width': 0.75,
-        'std': 0.1,
-        'mode': 'tabular',
-        'sample_around_instance': True,
-        'discretize_continuous': False,
-        'seed': 0,
-    },
-    'itg': {},
-    'control': {}
-}
-
-def fill_param_dict(method, param_dict, dataset_tensor):
-    """
-    Fills in the missing parameters for the given method with the default parameters
-    :param method: str, name of the method
-    :param param_dict: dict, parameter dictionary
-    :return: dict, filled parameter dictionary
-    """
-    # None case
-    param_dict = {} if param_dict is None else param_dict
-    
-    # Parameters requiring variables from IG and LIME 
-    if (method == 'ig') and ('baseline' not in param_dict):
-        param_dict['baseline'] = torch.mean(dataset_tensor, dim=0).reshape(1, -1).float()
-    elif (method == 'lime') and ('dataset_tensor' not in param_dict):
-        param_dict['data'] = dataset_tensor
-
-    # Fill in missing parameters
-    default_param_dict = default_param_dicts[method]
-    for key in default_param_dict.keys():
-        if key not in param_dict:
-            param_dict[key] = default_param_dict[key]
-    return param_dict
-
-def Explainer(method, model, dataset_tensor, param_dict=None):
+def Explainer(method, model, param_dict):
     """
     Returns an explainer object for the given method
     :param method: str, name of the method
     :param model: PyTorch model or function
-    :param dataset_tensor: torch.tensor, dataset tensor
-    :param param_dict: dict, parameter dictionary
+    :param param_dict: dict, __init__ parameters dictionary for the explainer
     :return: explainer object
     """
-    # Verify method
     if method not in explainers_dict.keys():
         raise NotImplementedError("This method has not been implemented, yet.")
-
-    # Configure parameters
-    model = model.predict if method == 'lime' else model
-    param_dict = fill_param_dict(method, param_dict, dataset_tensor)
-
-    # Return explainer
+    
     explainer = explainers_dict[method](model, **param_dict)
     return explainer
